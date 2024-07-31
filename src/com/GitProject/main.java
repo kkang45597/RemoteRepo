3eab6b7ab0ecdeb9b12673f0d6609183b61a40de
KMG <kkang45597@gmail.com>
2024-07-31 10:26:26 +0900

    local repo edit main.class

--- src/com/GitProject/main.java
+++ src/com/GitProject/main.java
@@ -4,9 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("main branch created main.class");
-		System.out.println("main branch edit main.class 1");
-		System.out.println("main branch edit main.class 3");
+		System.out.println("local repo edit main.class");
 
 	}
 
