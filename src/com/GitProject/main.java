348866532acc87850718229e54a5c4d6afe1fd33
KMG <kkang45597@gmail.com>
2024-07-31 10:18:43 +0900

    main branch edit main.class 1

--- src/com/GitProject/main.java
+++ src/com/GitProject/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		
 		System.out.println("main branch created main.class");
+		System.out.println("main branch edit main.class 1");
 
 	}
 
